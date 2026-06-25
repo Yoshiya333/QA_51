68311a9d81707e5449f94cd15afd1dcddfe74a37
Vladimir <derent145@gmail.com>
2026-06-25 21:13:20 +0500

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-}
-
-
-void main(String[] args) {
-
 }
